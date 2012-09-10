e2333eb49db24f2d5d08405d2111fc6d2c2d9a09
Gabriel Adomnicai <gabesoft@gmail.com>
2012-09-09 20:28:57 -0700

    initial commit

---
+++ test/transfer_test.js
@@ -0,0 +1,17 @@
+var should   = require('should')
+  , Transfer = require('../lib/sftpd/transfer')
+  , sut      = new Transfer('/a/b/c', '/a/e/f/d');
+
+describe('transfer', function() {
+    it('should generate source and destination paths for a file', function() {
+        var f1 = '/a/e/f/d/f1.js'
+          , f2 = '/a/e/f/d/p/f2.js';
+        sut.paths(f1).should.eql({ src: f1, dst: '/a/b/c/f1.js' });
+        sut.paths(f2).should.eql({ src: f2, dst: '/a/b/c/p/f2.js' });
+    });
+
+    it('should return null on invalid paths', function() {
+        var f1 = '/a/e/f/f1';
+        should.not.exist(sut.paths(f1));
+    });
+});
