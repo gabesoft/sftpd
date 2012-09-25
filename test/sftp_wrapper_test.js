fac69cf7c1f0975a4b85d34ed6a2b0ac0b5cfa23
Gabriel Adomnicai <gabesoft@gmail.com>
2012-09-24 22:12:00 -0700

    sftp fixes

--- test/sftp_wrapper_test.js
+++ test/sftp_wrapper_test.js
@@ -1,19 +1,35 @@
 var should = require('should')
+  , _      = require('underscore')
+  , fs     = require('fs')
+  , path   = require('path')
+  , temp   = require('temp')
+  , async  = require('async')
   , Log    = require('./log')
-  , Sftp   = require('../lib/sftpd/sftp_wrapper');
+  , SftpW   = require('../lib/sftpd/sftp_wrapper');
 
 describe('sftp_wrapper', function() {
+    var sftp = new SftpW('ftpuser', 'localhost', new Log());
+
     it('should create all directories in path (non root)', function(done) {
-        var sftp = new Sftp('ftpuser', 'localhost', new Log());
         sftp.mkdir('a/b/c/d', function() {
             done();
         });
     });
 
-    //it('should create all directories in path (root)', function(done) {
-    //var sftp = new Sftp('ftpuser', 'localhost', new Log());
-    //sftp.mkdir('/a/b/c/d', function() {
-    //done();
-    //});
-    //});
+    it('should upload multiple files', function(done) {
+        var data = 'temp data';
+        async.forEach(_.range(5), function(e, cb) {
+            temp.open('sftp_test',  function(err, info) {
+                fs.write(info.fd, data);
+                fs.close(info.fd, function(err) {
+                    sftp.put({
+                        src: info.path
+                      , dst: path.join('tmp', path.basename(info.path))
+                    }, cb);
+                });
+            });
+        }, function(err) {
+            sftp.bye(function(err2) { done(err || err2); });
+        });
+    });
 });
